9a1f1aa74266dbbc655612b768e9522fad6d0ffe
mehul-47 <mistrymehul.ele@gmail.com>
2022-06-20 18:04:52 +0530

    added changes

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 // extra packages
-
+app.get('/',(req,res)=>{
+  res.send('Jobs Api')
+}) 
 // routes
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticateUser,jobsRouter)
